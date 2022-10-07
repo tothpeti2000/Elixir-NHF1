a1fb5fd2556f18eae4aec4c32bbacaf9f7d7b6ff
tothpeti2000 <toth.peti26@gmail.com>
2022-10-07 19:23:34 +0200

    Initial commit

---
+++ lib/nhf1.ex
@@ -0,0 +1,18 @@
+defmodule Nhf1 do
+  @moduledoc """
+  Documentation for `Nhf1`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Nhf1.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nhf1_test.exs
@@ -0,0 +1,8 @@
+defmodule Nhf1Test do
+  use ExUnit.Case
+  doctest Nhf1
+
+  test "greets the world" do
+    assert Nhf1.hello() == :world
+  end
+end
