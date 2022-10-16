19e39bddbbab654333be6eca5c0d21f043fa4874
tothpeti2000 <toth.peti26@gmail.com>
2022-10-16 19:28:01 +0200

    Solution modified

--- lib/camping.ex
+++ lib/camping.ex
@@ -38,6 +38,7 @@ defmodule Nhf1 do
 
     map = init_tree_tent_map(trees)
     map = get_possible_tent_fields(map, row_count, col_count)
+
     find_solutions(map, tents_count_rows, tents_count_cols)
 
     # Get all possible directions
@@ -57,7 +58,7 @@ defmodule Nhf1 do
   end
 
   defp init_tree_tent_map(trees) do
-    for tree <- trees, into: %{}, do: {tree, []}
+    for tree <- trees, into: %{}, do: {tree, %{}}
   end
 
   defp get_possible_tent_fields(map, row_count, col_count) do
@@ -69,7 +70,8 @@ defmodule Nhf1 do
   defp get_tree_neighbors(tree, row_count, col_count, map) do
     for direction <- [:n, :e, :s, :w],
         (result = get_tree_neighbor(tree, direction, row_count, col_count, map)) !== nil,
-        do: result
+        into: %{},
+        do: {direction, result}
   end
 
   defp get_tree_neighbor({i, j}, :n, _row_count, _col_count, map) do
@@ -95,54 +97,45 @@ defmodule Nhf1 do
   defp find_solutions(tree_tent_map, tents_count_rows, tents_count_cols),
     do: find_solutions(tree_tent_map, tents_count_rows, tents_count_cols, [])
 
-  defp find_solutions(tree_tent_map, _tents_count_rows, _tents_count_cols, directions)
+  defp find_solutions(tree_tent_map, _tents_count_rows, _tents_count_cols, tents)
        when map_size(tree_tent_map) === 0,
-       do: directions
+       do: tents
 
   defp find_solutions(tree_tent_map, tents_count_rows, tents_count_cols, tents_so_far) do
-    IO.inspect(tree_tent_map)
+    # IO.inspect(tree_tent_map)
+    first_tree = Map.keys(tree_tent_map) |> hd()
+    tent_map = Map.get(tree_tent_map, first_tree)
 
-    [first_tree | other_trees] = Map.keys(tree_tent_map)
-
-    if length(Map.get(tree_tent_map, first_tree)) === 0 do
-      nil
-    else
-      [first_tent | other_tents] = Map.get(tree_tent_map, first_tree)
-
-      if violates_row_constraint?(first_tent, tents_so_far, tents_count_rows) ||
-           violates_col_constraint?(first_tent, tents_so_far, tents_count_cols) ||
-           touches_other_tent?(first_tent, tents_so_far) do
-        map = %{tree_tent_map | first_tree => other_tents}
-
-        result = find_solutions(map, tents_count_rows, tents_count_cols, tents_so_far)
-
-        if result === nil, do: tents_so_far, else: result
-      else
-        map = Map.delete(tree_tent_map, first_tree)
-
+    for {dir, tent} <- tent_map,
+        !violates_row_constraint?(tent, tents_so_far, tents_count_rows),
+        !violates_col_constraint?(tent, tents_so_far, tents_count_cols),
+        !touches_other_tent?(tent, tents_so_far),
         result =
-          find_solutions(map, tents_count_rows, tents_count_cols, tents_so_far ++ [first_tent])
-
-        if result === nil, do: tents_so_far, else: result
-      end
-    end
+          find_solutions(
+            Map.delete(tree_tent_map, first_tree),
+            tents_count_rows,
+            tents_count_cols,
+            tents_so_far ++ [{dir, tent}]
+          ),
+        length(result) !== 0,
+        do: result
   end
 
   defp violates_row_constraint?({i, _j}, tents_so_far, tents_count_rows) do
     expected_row_count = Enum.at(tents_count_rows, i - 1)
-    row_tents_so_far = tents_so_far |> Enum.filter(fn {a, _b} -> a === i end)
+    row_tents_so_far = tents_so_far |> Enum.filter(fn {_dir, {a, _b}} -> a === i end)
 
     expected_row_count >= 0 && length(row_tents_so_far) + 1 > expected_row_count
   end
 
   defp violates_col_constraint?({_i, j}, tents_so_far, tents_count_cols) do
     expected_col_count = Enum.at(tents_count_cols, j - 1)
-    col_tents_so_far = tents_so_far |> Enum.filter(fn {_a, b} -> b === j end)
+    col_tents_so_far = tents_so_far |> Enum.filter(fn {_dir, {_a, b}} -> b === j end)
 
     expected_col_count >= 0 && length(col_tents_so_far) + 1 > expected_col_count
   end
 
   defp touches_other_tent?({i, j}, tents_so_far) do
-    Enum.any?(tents_so_far, fn {a, b} -> abs(a - i) <= 1 && abs(b - j) <= 1 end)
+    Enum.any?(tents_so_far, fn {_dir, {a, b}} -> abs(a - i) <= 1 && abs(b - j) <= 1 end)
   end
 end
